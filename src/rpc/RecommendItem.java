ea9c392d7f3e71f46f9ade086eb85c3ba638b114
unknown <gaojiaxi2015@gmail.com>
2020-03-21 18:14:09 +0800

    add project backbone

---
+++ src/rpc/RecommendItem.java
@@ -0,0 +1,55 @@
+package rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Servlet implementation class RecommendItem
+ */
+@WebServlet("/recommendation")
+public class RecommendItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RecommendItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//tell front end the data type is json
+		JSONArray array = new JSONArray();
+		try {
+			array.put(new JSONObject().put("name", "abcd").put("address", "san francisco").put("time", "01/01/2017"));
+			array.put(new JSONObject().put("name", "1234").put("address", "san jose").put("time", "01/02/2017"));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		RpcHelper.writeJsonArray(response, array);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
