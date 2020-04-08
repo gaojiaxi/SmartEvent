9ac576b90b6844d8863c0d3815789607f8df0b85
unknown <gaojiaxi2015@gmail.com>
2020-04-08 22:05:59 +0800

    add database connection using JDBC

---
+++ src/db/DBConnectionFactory.java
@@ -0,0 +1,22 @@
+package db;
+
+public class DBConnectionFactory {
+	private static final String DEFAULT_DB = "mysql";
+	public static DBConnection getConnection(String db) {
+		switch(db) {
+		case "mysql":
+//			return new MySQLConnection();
+			return null;
+		case "mongodb":
+//			return new MongoDBConnection();
+			return null;
+		default:
+			throw new IllegalArgumentException("Invalid db: " + db);
+		}
+	}
+	
+	public static DBConnection getConnection() {
+		return getConnection(DEFAULT_DB);
+	}
+	
+}
